7affe427895825fa0eae613c44f550a7792657fe
Aryaman Singh <arya.ribharya@gmail.com>
2020-09-05 15:26:51 +0530

    Removed redundant code

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 // ?retryWrites=true&w=majority
 app.use(express.static("public"));
-mongoose.connect("mongodb+srv://admin-arya:arya@cluster0.ob7wg.mongodb.net/wordictDB",{ useUnifiedTopology: true ,useNewUrlParser:true}).catch(err => console.log(err));
+mongoose.connect("mongodb+srv://admin-arya:arya@cluster0.ob7wg.mongodb.net/wordictDB", { useUnifiedTopology: true, useNewUrlParser: true }).catch(err => console.log(err));
 const usersSchema = new mongoose.Schema({
     email: String,
     password: String,
@@ -34,66 +34,33 @@ var foundmeanings = [];
 
 
 app.get("/", function (req, res) {
-    
+
     var name = "WORDict"
-    suser={};
+    suser = {};
     console.log(suser);
     res.render("home", { varname: name });
 })
 app.get("/main", function (req, res) {
-   
-    if(suser.email==="")
-    res.redirect("/");
+
+    if (suser.email === "")
+        res.redirect("/");
 
     if (suser) {
-      
+
         res.render("main", { wl: words, ml: foundmeanings });
-        console.log("in /main "+words);
+        console.log("in /main " + words);
         console.log(foundmeanings);
-
-        // console.log(words.length + "\n");
-    
-        // for (var i = 0; i < suser.userwords.length; i++) {
-           
-            
-        //    Word.findOne({word:suser.userwords[i]},function(err,foundWord){
-        //        if(!err){
-        //            foundmeanings.push(foundWord.definitions);
-        //        }
-        //        else{
-        //            console.log(err);
-        //        }
-
-        //    })
-
-
-        // }
-     
-        
-
-        
-        
-
-
-        
-        // res.redirect("/main");
-
-
-
 
 
 
     }
     else {
-        if(suser.email==="")
-        res.redirect("/");
+        if (suser.email === "")
+            res.redirect("/");
     }
 
 })
-// app.patch("/main", function (req, res) {
 
-
-// })
 app.post("/main", function (req, res) {
 
 
@@ -111,134 +78,93 @@ app.post("/main", function (req, res) {
         res.send("word is already present");
     }
     else {
-        meanings=[];
-        // var varname;
-      
+        meanings = [];
 
 
-        var uuid = require("uuid");
-        var id = uuid.v4();
 
 
 
         const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
-        https.get(url,function (response) {
+        https.get(url, function (response) {
 
             console.log(response.statusCode);
-            if(response.statusCode===404){
+            if (response.statusCode === 404) {
                 res.redirect("/api-err")
             }
-             if(response.statusCode===429){
-                
+            if (response.statusCode === 429) {
+
                 res.redirect("/api-err");
             }
-             
-        
-            if(response.statusCode===200)
-           {  words.push(word);
-               User.updateOne({ email: suser.email }, { $set: { userwords: words } }, function (err) {
-            if (!err) {
-                console.log("updated");
 
-            }
-            else {
-                res.send(err);
-            }
 
-        });
-               response.on("data", function (data) {
-                wordData = JSON.parse(data);
-                console.log(wordData);
-                wordData = wordData[0]["meanings"];
-                
-                meanings.push(wordData[0]["definitions"][0]["definition"]);
-                // });
-                console.log(meanings);
-                const newWord = new Word({
-                    word: word,
-                    definitions: wordData[0]["definitions"][0]["definition"],
-                });
-                newWord.save(function (err) {
+            if (response.statusCode === 200) {
+                words.push(word);
+                User.updateOne({ email: suser.email }, { $set: { userwords: words } }, function (err) {
                     if (!err) {
-                        console.log("word Data added to database");
-                        console.log(words);
-                        
-
-                     
-
-                        
-                        Word.findOne({ word: word }, function (err, foundWord) {
-                            if (!err) {
-                                console.log(foundWord.word);
-                             
-                                
-                                foundmeanings.push(foundWord.definitions);
-                                console.log(foundmeanings);
-                                console.log(foundWord.definitions);
-                                if(foundmeanings.length===words.length){
-                                    res.render("main",{wl:words,ml:foundmeanings});
-                                }
-                            }
-                            else {
-                                console.log(err);
-                            }
-                        })
-
+                        console.log("updated");
 
                     }
                     else {
-                        console.log(err);
-                        // res.send(err);
+                        res.send(err);
                     }
-                })
-            
+
+                });
+                response.on("data", function (data) {
+                    wordData = JSON.parse(data);
+                    console.log(wordData);
+                    wordData = wordData[0]["meanings"];
+
+                    meanings.push(wordData[0]["definitions"][0]["definition"]);
+                    // });
+                    console.log(meanings);
+                    const newWord = new Word({
+                        word: word,
+                        definitions: wordData[0]["definitions"][0]["definition"],
+                    });
+                    newWord.save(function (err) {
+                        if (!err) {
+                            console.log("word Data added to database");
+                            console.log(words);
 
 
 
 
 
+                            Word.findOne({ word: word }, function (err, foundWord) {
+                                if (!err) {
+                                    console.log(foundWord.word);
 
 
+                                    foundmeanings.push(foundWord.definitions);
+                                    console.log(foundmeanings);
+                                    console.log(foundWord.definitions);
+                                    if (foundmeanings.length === words.length) {
+                                        res.render("main", { wl: words, ml: foundmeanings });
+                                    }
+                                }
+                                else {
+                                    console.log(err);
+                                }
+                            })
 
 
+                        }
+                        else {
+                            console.log(err);
+                            // res.send(err);
+                        }
+                    })
 
-            });}
-         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                });
+            }
         });
-    
-
-
-
-
-
-
-
     }
-
-
 });
+
 app.get("/check", function (req, res) {
     res.render("check", { meaningList: meanings });
 })
-app.get("/api-err",function(req,res){
+app.get("/api-err", function (req, res) {
     res.render("api_err");
 })
 
@@ -247,63 +173,63 @@ app.get("/mismatch", function (req, res) {
 })
 
 app.get("/login", function (req, res) {
-    suser={};
+    suser = {};
     console.log(suser);
     res.render("login");
 })
 
 app.post("/", function (req, res) {
-    
-    if(req.body.pass1.length===0||req.body.pass2.length===0){
+
+    if (req.body.pass1.length === 0 || req.body.pass2.length === 0) {
         console.log("Password error");
         res.redirect("/mismatch");
-    
-    }
-    else{
-    var email = req.body.username;
-    var password = md5(req.body.pass1);
-    var confirm = md5(req.body.pass2);
-    console.log(password.length);
 
-    if (password != confirm) {
-        console.log("passwords error")
-        res.redirect("/mismatch");
-        return;
     }
+    else {
+        var email = req.body.username;
+        var password = md5(req.body.pass1);
+        var confirm = md5(req.body.pass2);
+        console.log(password.length);
 
-    
-    User.findOne({ email: email }, function (err, foundUser) {
-        if (err) {
-            console.log(err);
+        if (password != confirm) {
+            console.log("passwords error")
+            res.redirect("/mismatch");
+            return;
         }
-        else {
-            if (foundUser) {
-                if (foundUser.password == password) {
-                    console.log("user already exists, login instead");
-                    res.send("You already have an account");
-                }
-                else{
-                    res.send("Your account already exists");
-                }
+
+
+        User.findOne({ email: email }, function (err, foundUser) {
+            if (err) {
+                console.log(err);
             }
             else {
-                const newUser = new User({
-                    email: email,
-                    password: password
-                });
-                newUser.save(function (err) {
-                    if (err) {
-                        console.log(err);
+                if (foundUser) {
+                    if (foundUser.password == password) {
+                        console.log("user already exists, login instead");
+                        res.send("You already have an account");
                     }
                     else {
-                        // res.send("created account Successfully");
-                        res.redirect("/login");
+                        res.send("Your account already exists");
                     }
-                })
+                }
+                else {
+                    const newUser = new User({
+                        email: email,
+                        password: password
+                    });
+                    newUser.save(function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            // res.send("created account Successfully");
+                            res.redirect("/login");
+                        }
+                    })
 
+                }
             }
-        }
-    })
+        })
 
     }
 
@@ -324,57 +250,56 @@ app.post("/login", function (req, res) {
                 if (foundUser.password == password) {
                     suser = foundUser;
                     console.log(suser);
-                    console.log(typeof(suser));
+                    console.log(typeof (suser));
                     words = [];
-                    cords=[];
-                    foundmeanings=[];
-                    meanings=[];
-                  uflag=1;
-                  console.log(suser.userwords.length);
-                  if(suser.userwords.length==0){
-                      res.redirect("/main");
-                  }
-                  else{
-                  if(uflag===1){
-                      for(var i=0;i<suser.userwords.length;i++){
-                        cords.push(suser.userwords[i]);
-                      }
-                      console.log(cords);
-                    for(var j=0;j<cords.length;j++)
-                    {
-                        console.log(cords[j]);
-                        console.log(j);
-                        
-                        
-                        Word.findOne({word:cords[j]},function(err,usr){
-                            // console.log(words[j]);
-                            
-                            console.log("in login "+usr.definitions);
-                            words.push(usr.word);
-                            foundmeanings.push(usr.definitions);
-                            console.log(foundmeanings);
-                            if(foundmeanings.length===cords.length){
-                                console.log(foundmeanings);
-                                uflag=0;
-                                console.log(i);
-                                 res.redirect("/main");
-                                //  console.log("sent");
-                               
-                            
-                            }
-
-                        
-                           if(err){
-                               console.log(err);
-                           }
-                        })
+                    cords = [];
+                    foundmeanings = [];
+                    meanings = [];
+                    uflag = 1;
+                    console.log(suser.userwords.length);
+                    if (suser.userwords.length == 0) {
+                        res.redirect("/main");
                     }
-                }
-                    
+                    else {
+                        if (uflag === 1) {
+                            for (var i = 0; i < suser.userwords.length; i++) {
+                                cords.push(suser.userwords[i]);
+                            }
+                            console.log(cords);
+                            for (var j = 0; j < cords.length; j++) {
+                                console.log(cords[j]);
+                                console.log(j);
 
-                }
-                  
-                    
+
+                                Word.findOne({ word: cords[j] }, function (err, usr) {
+                                    // console.log(words[j]);
+
+                                    console.log("in login " + usr.definitions);
+                                    words.push(usr.word);
+                                    foundmeanings.push(usr.definitions);
+                                    console.log(foundmeanings);
+                                    if (foundmeanings.length === cords.length) {
+                                        console.log(foundmeanings);
+                                        uflag = 0;
+                                        console.log(i);
+                                        res.redirect("/main");
+                                        //  console.log("sent");
+
+
+                                    }
+
+
+                                    if (err) {
+                                        console.log(err);
+                                    }
+                                })
+                            }
+                        }
+
+
+                    }
+
+
                 }
                 else {
 
@@ -390,12 +315,11 @@ app.post("/login", function (req, res) {
     })
 })
 
-let port=process.env.PORT;
-if(!port)
-   { port=3000;}
+let port = process.env.PORT;
+if (!port) { port = 3000; }
 
 
 app.listen(port, function (res) {
     console.log("Server ready ");
-    
+
 })
